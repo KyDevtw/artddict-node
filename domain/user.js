c5ef9c8bb9fd1972fa8124e0286d15a993fe1499
chun2332 <smile84122332@gmail.com>
2021-07-10 19:02:39 +0800

    修改資料、密碼完成

--- domain/user.js
+++ domain/user.js
@@ -11,7 +11,7 @@ class User {
     this.userImg = userImg;
     this.userfav = userfav;
     this.userCoupon = userCoupon;
-    this.login = 0
+    this.login = 0;
   }
 
   addUserSQL() {
@@ -20,12 +20,56 @@ class User {
     return sql
   }
 
+  // updateUserByIdSQL(id) {
+  //   let sql = `UPDATE USERS \
+  //              SET username = '${this.username}', name = '${this.name}', gender = '${this.gender}', mobile = '${this.mobile}' , birthday = '${this.birthday}', address = '${this.address}', login = ${this.login} \
+  //              WHERE id =  ${id}`
+    
+  //   return sql
+  // }
+
   updateUserByIdSQL(id) {
+    let sql = `UPDATE USERS \ SET `
+    if (this.username != null) {
+      sql += `username = '${this.username}',`
+    }
+    if (this.name != null) {
+      sql += `name = '${this.name}', `
+    }
+    if (this.gender != null ) {
+      sql += `gender = '${this.gender}', `
+    }
+    if (this.mobile != null) {
+      sql += `mobile = '${this.mobile}' , `
+    }
+    if (this.birthday != null) {
+      sql += `birthday = '${this.birthday}', `
+    }
+    if (this.address != null) {
+      sql += `address = '${this.address}', `
+    } 
+    sql += `login = ${this.login} \ WHERE id = ${id}`
+      
+    
+    return sql
+  }
+
+  updatePwdByIdSQL(id) {
+    console.log(`pwd received = ${this.password}`)
     let sql = `UPDATE USERS \
-               SET username = '${this.username}', name = '${this.name}', password = '${this.password}', mobile = '${this.mobile}',  gender = '${this.gender}' , address = '${this.address}', login = ${this.login} \
+               SET password = '${this.password}', login = ${this.login} \
                WHERE id =  ${id}`
     return sql
   }
+
+  // updatePwdByIdSQL(id) {
+  //   let sql = `UPDATE USERS \ SET`
+  //   if (this.password != null) {
+  //     sql += `password = '${this.password}\ WHERE id = ${id}`
+  //   }
+  // }
+
+
 
   // static是與實例化無關
   static getUserByIdSQL(id) {
